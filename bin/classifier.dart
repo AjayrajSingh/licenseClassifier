e9ce7a5387be75733de7ca1fe7b2db86fd8d69a5
AjayrajSingh <shiningcoders@gmail.com>
2021-03-29 22:59:02 +0530

    Added comments

--- bin/classifier.dart
+++ bin/classifier.dart
@@ -2,10 +2,13 @@ import '../lib/utils/licenseListParser.dart';
 import '../lib/utils/licenseParser.dart';
 import 'dart:convert';
 
+/// Entrypoint to dart package
 Future<void> main() async {
   LicenseListParser licenseListParser = LicenseListParser();
   await licenseListParser.fetchLicenseJsonList();
+
+  /// Passing url of first fetched license file to licenseParser
   LicenseParser licenseParser =
-      LicenseParser(url: licenseListParser.licenseList[77]['url']);
+      LicenseParser(url: licenseListParser.licenseList[0]['url']);
   await licenseParser.fetchLicenseJsonList();
 }
