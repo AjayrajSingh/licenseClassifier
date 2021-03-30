f7f98ef055b66af1029f026685a671876845824b
AjayrajSingh <shiningcoders@gmail.com>
2021-03-31 02:17:39 +0530

    Changed signatures and added comments

--- example/main.dart
+++ example/main.dart
@@ -1,13 +1,19 @@
 import 'package:classifier/classifier.dart';
 import 'dart:convert';
 
+// Below given implementation is temprary, LicenseList and LicenseParser
+// classes will only be exposed to the Matcher class
 /// Entrypoint to dart package
 Future<void> main() async {
-  LicenseListParser licenseListParser = LicenseListParser();
-  await licenseListParser.fetchLicenseJsonList();
+  LicenseList licenseListFetcher = LicenseList();
+  await licenseListFetcher.fetchLicenseList();
 
   /// Passing url of first fetched license file to licenseParser
+  ///
+  // Parse Tree isn't done yet, only first license can be printed in this list as of now
   LicenseParser licenseParser =
-      LicenseParser(url: licenseListParser.licenseList[0]['url']);
-  await licenseParser.fetchLicenseJsonList();
+      LicenseParser(url: licenseListFetcher.licenseList[0]['url']);
+  await licenseParser.fetchLicense();
+
+  licenseParser.printLicense(xmlContent: licenseParser.content);
 }
