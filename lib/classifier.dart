f4e286ae6fe3ee9c96985b6a27b170930dba5e5f
AjayrajSingh <shiningcoders@gmail.com>
2021-03-31 02:16:44 +0530

    Added comments

--- lib/classifier.dart
+++ lib/classifier.dart
@@ -1,2 +1,5 @@
-export './src/licenseListParser.dart';
+// These exports are temprary this file will only be exporting
+// the matcher.dart file
+
+export './src/licenseList.dart';
 export './src/licenseParser.dart';
