940ecbc80f0274a2fce9467139ce25592e7366e5
AjayrajSingh <shiningcoders@gmail.com>
2021-04-03 23:26:07 +0530

    Defined matcher methods

--- lib/src/matcher.dart
+++ lib/src/matcher.dart
@@ -1,2 +1,22 @@
 /// Provides matchers method to detect the license
-class Matcher {}
+class Matcher {
+  String inlineLicense = '';
+
+  Matcher({
+    this.inlineLicense,
+  });
+
+  //TODO: make the match, checking the license have no further information
+  /// Checks if license have copyright notice and nothing else
+  String headerMatch() {
+    if (RegExp('.*(c).*\.').hasMatch('inlineLicense')) {
+      return 'none';
+    }
+  }
+
+  /// Checks if the license have an exact match in the license list
+  extactMatch() {}
+
+  /// Using dice-coefficient checks the percentage of matching license
+  diceMatch() {}
+}
