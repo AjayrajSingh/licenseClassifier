74d35ada87f9bebb57bb7787bcaa256816828b2a
AjayrajSingh <shiningcoders@gmail.com>
2021-04-04 22:01:56 +0530

    Added TODOs

--- lib/src/matcher.dart
+++ lib/src/matcher.dart
@@ -15,9 +15,11 @@ class Matcher {
     }
   }
 
+  // TODO: Implement the brute-force for checking exact match in the license list
   /// Checks if the license have an exact match in the license list
   extactMatch() {}
 
+  // TODO: Implement the Sorensen dice-coefficient
   /// Using dice-coefficient checks the percentage of matching license
   diceMatch() {}
 }
